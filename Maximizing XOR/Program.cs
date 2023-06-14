2ed37a4e349a0a56155400f81de563b3b191c1bc
Mohammed Seoud <m.seoud42@gmail.com>
2023-06-15 02:27:16 +0300

    Add project files.

---
+++ Maximizing XOR/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+    // Complete the maximizingXor function below.
+    static int maximizingXor(int l, int r)
+    {
+        int ret = 0;
+        for (int i = l; i <= r; i++)
+        {
+            for (int j = l; j <= r; j++)
+            {
+                 long num = i ^ j;
+                if ( num> ret) 
+                {
+                    ret = (int)num;
+                }
+            }
+        }
+        return ret;
+    }
+
+    static void Main(string[] args)
+    {
+     //   TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int l = Convert.ToInt32(Console.ReadLine());
+
+        int r = Convert.ToInt32(Console.ReadLine());
+
+        int result = maximizingXor(l, r);
+
+        //textWriter.WriteLine(result);
+
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
